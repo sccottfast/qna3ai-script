2e72869b9592fa46e22c4d80f0317f122ccaf404
sccottfast <99705845+sccottfast@users.noreply.github.com>
2024-01-16 10:12:56 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,18 @@ import { Wallet } from "ethers";
 import { Qna3 } from "./scirpts/qna3ai";
 
 (async () => {
-   var  ags=process.argv.splice(2)
-  const wallet = new Wallet(ags[0]);
-  const qna3 = new Qna3(wallet, 206);
-  await qna3.login(ags[1]);
-  await qna3.checkIn();
+   var  ags= await process.argv
+
+  console.error(ags[2])
+  console.error(ags[3])
+
+  if(ags[2] && ags[3]){
+    const wallet = new Wallet(ags[2]);
+     const qna3 = new Qna3(wallet, 206);
+    await qna3.login(ags[3]);
+    await qna3.checkIn();
+  }
+  
+
+
 })();
